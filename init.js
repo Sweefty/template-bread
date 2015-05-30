4f157231082b934311b9cfad012c87b4aec4f8e6
mamod <mamod.mehyar@gmail.com>
2015-05-30 21:09:05 +0300

    first commit

---
+++ init.js
@@ -0,0 +1,47 @@
+
+exports.outLocation = '';
+
+//handlebars partials
+exports.partials = {};
+
+//handlebars helpers
+exports.helpers = {
+    test : function(arg1){}
+};
+
+//handlebars data
+exports.data = {
+    "javascript" : [],
+    
+    "fonts" : [
+        "https://fonts.googleapis.com/css?family=Dosis",
+    ],
+
+    "description" : "beautiful free template",
+    "title" : "bread template",
+
+    "logo" : {
+        "src" : "./assets/images/logo.png",
+        "alt" : "Sweefty",
+        "url" : "#top" //optional
+    },
+
+    //spash options
+    "splash" : {
+        "head"    : "Hello, I'm Bread",
+        "subhead" : "A very lovely responsive, Free HTML5 Template.. Don't you think?",
+        "buttons" : [
+            {
+                "text" : "Learn More",
+                "link" : "#more"
+            },
+            {
+                "text" : "Get Now",
+                "link" : "#buy"
+            },
+        ]
+    },
+
+    //optional -- comment out to remove
+    // "ourClients" : "Our Clients"
+};
